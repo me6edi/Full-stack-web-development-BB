51af9c9918e098b217e894d937ab43d5b5990efe
me6edi <exited17@gmail.com>
2023-05-08 22:15:47 +0600

    javascript outputs,connect js file to html,statement,syntax,comments,user input

---
+++ Modern JavaScript Programming/js/script.js
@@ -0,0 +1,14 @@
+
+// console.log("Hello!");
+// alert("Hello");
+// document.write("Hey mehedi");
+// window.alert("How are you?");
+
+/*document.write(50)
+document.write(40)
+console.log(32)*/
+
+// var x;
+// x = prompt("Enter your name: ");
+// document.write(x);
+// console.log(x);
