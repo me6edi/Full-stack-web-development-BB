be092b81589a8404b32b57a089232e1e53522ac6
me6edi <exited17@gmail.com>
2023-05-09 21:56:10 +0600

    Variabls,Constants,Keywords

--- Modern JavaScript Programming/js/script.js
+++ Modern JavaScript Programming/js/script.js
@@ -12,3 +12,63 @@ console.log(32)*/
 // x = prompt("Enter your name: ");
 // document.write(x);
 // console.log(x);
+
+// Variable Declaration
+
+// var myName;
+// myName = "Mehedi Amin";
+// console.log(myName);
+// var myAge = 20;
+// document.write(myAge);
+
+// var a;
+// let b;
+// a = 45;
+// console.log(a);
+// a = 35.34;
+// b = "Mehedi Amin";
+// a = "Rakib";
+// console.log(a);
+// console.log(b); 
+
+// var a;
+// let b;
+// a = 45;
+// console.log(a);
+// a = 35.34;
+// console.log(a);
+// a = "Mehedi Amin";
+// console.log(a);
+
+// var a = 20;
+// var b = a;
+// console.log(a);
+// console.log(b);
+// a = 30;
+// console.log(a);
+// console.log(b);
+
+// var a = 20;
+// var b = 10;
+// var c = a + b;
+// console.log(c);
+
+// var $a;
+// $a = 20;
+// console.log($a);
+
+// var _abc = 30;
+// console.log(_abc);
+
+// var myName,myname
+// myName = 20;
+// myname = 30;
+// console.log(myName,myname);
+
+// var a = 20;
+// console.log(a);
+// var a;
+// console.log(a);
+
+// const a = 2;
+// console.log(a)
