1b978267562dc2b1d9be598b7b594747675446bd
me6edi <exited17@gmail.com>
2023-05-17 21:48:29 +0600

    Operators arithmetic,comparison,logical,conditional assignment

--- Modern JavaScript Programming/js/script.js
+++ Modern JavaScript Programming/js/script.js
@@ -72,3 +72,95 @@ console.log(32)*/
 
 // const a = 2;
 // console.log(a)
+
+// Operator
+// Binary operator work tow operand
+// arithmetic operator +,-,*,/,%
+
+// var a = 2;
+// var b = 2;
+// document.write(a+b);
+// document.write("<br>",a-b);
+// document.write("<br>",a*b);
+// document.write("<br>",a**b);
+
+// increment operator and dicremnt operator a++ or ++a or --a or a--
+// var a = 20;
+// var b = 30;
+// a++;
+// ++b;
+// console.log(a)
+// console.log(b)
+
+// var num1 = 5;
+// var num2 = 30;
+
+// var num3,num4;
+
+// num3 = num1++;
+// num4 = ++num2;
+
+// console.log(num3);
+// console.log(num1);
+// console.log(num2);
+
+// var a = (5-3) * 5 / 7;
+// console.log(a)
+
+// String operator
+
+// var  line1 = "Hello";
+// var  line2 = "World";
+// console.log(line1 + " "+ line2);
+
+// value = line1 + 30;
+// value2 = "Hello" + 50 + 60;
+// value3 = 505 + 50 + "Hi"
+// console.log(value)
+// console.log(value2)
+// console.log(value3)
+
+// var a = "75";
+// var b = "20";
+// console.log(a-b);
+
+// var c = "75";
+// var d = "20";
+// console.log(c+d);
+
+// Comparison operators
+
+// var a = 20;
+// var b = 30;
+
+// var a = 20;
+// var b = 20;
+// console.log(a==b);
+
+// value,type or not equal compaire ==,=== or != 
+//  value1 = a == "4"
+//  value2 = a === "4";
+//  value3 = a != 5;
+// console.log(value1,value2,value3)
+
+// value = a > 5;
+// value2 = a < 1;
+// value3 = a >= 10
+// console.log(value,value2,value3)
+// var d = (5<4)
+// console.log(d)
+
+//logical operators ||,&&,!
+// (4>6) || (1<7)
+// (4>6) && (1<7)
+
+//conditional operators
+
+// var  a = (1>8) ? "Hello":"World";
+
+// console.log(a);
+
+//Assignment Operator
+// var a = 20;
+// a += 12;
+// console.log(a);
