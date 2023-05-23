5b83a3aacee9d0bc2d1933a934cd14abf9bdb3f0
me6edi <exited17@gmail.com>
2023-05-23 21:07:55 +0600

    python operators

---
+++ Programming with Python/hello_world.py
@@ -0,0 +1,57 @@
+# print("Hello world!")
+# usar_name = "Mehedi"
+# print(usar_name)
+# message = "Hello world!"
+# message = "This is a test message"
+# print(message)
+
+# Data type int,float,string..
+# Type_casting
+# total_data = "123.50"
+# total_data = float()
+# total_data = float("123.50")
+# print(type(total_data))
+
+# test_data = 2.50
+# test_data = str(test_data)
+# print(test_data)
+
+# assignment_operator 
+# numbers = 10
+# numbers2 = 20
+# numbers += numbers2
+# print(numbers)
+# number1 = 50
+# numbers2 = 20
+# number1 -= numbers2
+# number1 *= numbers2
+# print(number1) 
+
+# Comparison Operator
+
+# message = "Hellow world!"
+# message2 = "Hellow world!"
+# value1 = 20
+# value2 = 25
+# print(message <= message2)
+# print(message >= message2)
+
+# logical operator
+
+# print(value1 < value2 and message > message2)
+# print(value1 < value2 or message > message2)
+
+# identity_operator
+# number1 = 20
+# number2 = 30
+# print(id(number1))
+# print(id(number2))
+
+# print(number1 is number2)
+
+
+# my_list = []
+# my_list2 = []
+
+# print(my_list == my_list2)
+# print(my_list is my_list2)
