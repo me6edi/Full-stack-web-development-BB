493299db167ef431e348e38b50b6e4f188bca2bc
me6edi <exited17@gmail.com>
2023-06-01 20:44:32 +0600

    Result

--- Modern JavaScript Programming/js/script.js
+++ Modern JavaScript Programming/js/script.js
@@ -477,3 +477,57 @@ console.log(32)*/
 //     console.log("Invalid Number");
 // }
 
+// let number = prompt("What is you number?");
+// number = parseInt(number);
+
+// if(number >= 80 && number <= 100){
+//      console.log("A+");
+// }else if(number >= 75 && number <= 80){
+//     console.log("A");
+// }else if(number >= 70 && number <= 75){
+//     console.log("A-");
+// }else if(number >= 65 && number <= 70){
+//     console.log("B+");
+// }else if(number >= 60 && number <= 65){
+//     console.log("B");
+// }else if(number >= 55 && number <= 60){
+//     console.log("B-");
+// }else if(number >= 50 && number <= 55){
+//     console.log("C+");
+// }else if(number >= 45 && number <= 50){
+//     console.log("C");
+// }else if(number >= 40 && number <= 45){
+//     console.log("D");
+// }else if(number >= 0 && number <= 40){
+//     console.log("Fail!");
+// }else{
+//     console.log("Invalid Number");
+// }
+
+
+// var number = prompt("What is your number?");
+// number = parseInt(number);
+// let grade;
+
+// if(number <=100 && number >= 80){
+//     grade = "A+";
+// }else if(number < 80 && number >= 70){
+//     grade = "A";
+// }else if(number < 70 && number >= 60){
+//     grade = "A-";
+// }
+// else if(number < 60 && number >= 50){
+//     grade = "B";
+// }
+// else if(number < 50 && number >= 40){
+//     grade = "C";
+// }
+// else if(number < 40 && number >= 33){
+//     grade = "D";
+// }else if(number < 33 && number >= 0){
+//     grade = "F";
+// }else{
+//     grade = "Invalid number!";
+// }
+// console.log(grade);
+
