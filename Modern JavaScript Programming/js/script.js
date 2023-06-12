ce8128b421a28ce3a9610127d320f17d9c53da89
me6edi <exited17@gmail.com>
2023-06-12 21:32:27 +0600

    Sub Class

--- Modern JavaScript Programming/js/script.js
+++ Modern JavaScript Programming/js/script.js
@@ -975,3 +975,118 @@ console.log(32)*/
 // console.log(customer1);
 // console.log(customer1.greeting());
 // console.log(customer1.fullname()); 
+
+// Class(ES6)
+// Class is a template for creating objects
+// let person1 ={
+//     firstname: 'Mehedi',
+//     lastname: 'Amin',
+//     dob: '11-12-1999',
+
+//     fullname: function(){
+//         console.log(`${this.firstname} ${this.lastname}`);
+//     }
+// }
+
+// let person2 ={
+//     firstname: 'Robin',
+//     lastname: 'Amini',
+//     dob: '11-12-1909',
+
+//     fullname: function(){
+//         console.log(`${this.firstname} ${this.lastname}`);
+//     }
+// }
+
+// console.log(person1);
+// console.log(person2);
+
+// class Person{
+//     constructor(fname,lname,birthday){
+//         this.firstname = fname;
+//         this.lastname = lname;
+//         this.dob = birthday;
+//     }
+//     calculateAge(){
+//         let birthday = new Date(this.dob);
+//         let diff = Date.now() - birthday.getTime();
+//         let ageDate = new Date (diff);
+//         return Math.abs(ageDate.getUTCFullYear() - 1970);
+//     }
+// }
+
+// let person1 = new Person("Mehedi","Amin","19-12-2015");
+// let person2 = new Person("Hassn","Amin","19-12-2010");
+// let person3 = new Person("Robin","Amin","19-12-2000");
+// console.log(person1.calculateAge());
+// console.log(person2.calculateAge());
+// console.log(person3.calculateAge());
+
+// Class(ES6) 
+// Class is a template for creating objects
+
+// class Person {
+//     constructor(fname, lname, birthday) {
+//         this.firstname = fname;
+//         this.lastname = lname;
+//         this.dob = birthday;
+//     }
+
+//     calculateAge() {
+//         let birthday = new Date(this.dob);
+//         let diff = Date.now() - birthday.getTime();
+//         let ageDate = new Date(diff);
+//         return Math.abs(ageDate.getUTCFullYear() - 1970);
+//     }
+
+//     fullname() {
+//         console.log(this.firstname, this.lastname);
+//     }
+
+// }
+
+// let person1 = new Person("Fazle", "Rahat", 11-13-1988); 
+// let person2 = new Person("Moinul", "Islam", 02-11-1991);
+// let person3 = new Person("Rony", "Chy", 01-17-1492);
+
+// console.log(person1.calculateAge());
+// console.log(person2.calculateAge());
+// console.log(person3.calculateAge());
+
+
+// Sub Class
+// Inheritance
+
+// class Person { //Base Class
+//     constructor(fname, lname) {
+//         this.firstname = fname;
+//         this.lastname = lname;
+//     }
+
+//     greeting() {
+//         return `Hello ${this.firstname} ${this.lastname}!`;
+//     }
+// }
+
+// class Customer extends Person { //Sub Class
+//     constructor(fname, lname, phone, memberShip) {
+//         super(fname, lname);
+
+//         this.phone = phone;
+//         this.memberShip = memberShip;
+//     }
+
+//     fullname() {
+//         console.log(this.firstname, this.lastname);
+//     }
+// }
+
+// let person1 = new Person("Fazle", "Rahat");
+// console.log(person1);
+// console.log(person1.greeting());
+// console.log(person1.fullname());
+
+// let customer1 = new Customer("Rony", "Chy", "017777778888", "1234");
+// console.log(customer1);
+// console.log(customer1.greeting());
+// console.log(customer1.fullname()); 
