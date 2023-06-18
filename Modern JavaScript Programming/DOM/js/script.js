289b4446d94c99196cbf902c3d1d27c62ed76381
me6edi <exited17@gmail.com>
2023-06-18 23:11:12 +0600

    DOM Selector Single Element

--- Modern JavaScript Programming/DOM/js/script.js
+++ Modern JavaScript Programming/DOM/js/script.js
@@ -1,43 +1,60 @@
+// let val;
+// val = this;
+// val = window;
+// val = window.document;
+// val = document;
+// val = document.all;
+// val = document.all[2];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].href;
+// val = document.links[0].className;
+// val = document.links[0].classList;
+
+// val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[0];
+// val = document.scripts[0].src;
+// val = document.scripts[0].getAttribute('src');
+
+// links = document.links;
+// let linkArr = Array.from(links);
+
+// linkArr.forEach(function(link){
+//     console.log(link.getAttribute('href'));
+// });
+
+// console.log(links);
+// console.log(val);
+
+
+
+
+
+// DOM Selector Single Element
+
 let val;
-val = this;
-val = window;
-val = window.document;
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].method;
-val = document.forms[0].action;
-
-val = document.links;
-val = document.links[0];
-val = document.links[0].href;
-val = document.links[0].className;
-val = document.links[0].classList;
-
-val = document.images;
-
-val = document.scripts;
-val = document.scripts[0];
-val = document.scripts[0].src;
-val = document.scripts[0].getAttribute('src');
-
-links = document.links;
-let linkArr = Array.from(links);
-
-linkArr.forEach(function(link){
-    console.log(link.getAttribute('href'));
-});
-
-console.log(links);
+// Gettting an Element
+val = document.getElementById("dcument-title");
+val = document.getElementById("dcument-title").id;
+val = document.getElementById("dcument-title").className;
+val = document.getElementById("dcument-title").style.background = '#554';
+val = document.getElementById("dcument-title").style.color = '#fff';
+val = document.getElementById("dcument-title").style.padding = '20px';
 console.log(val);
