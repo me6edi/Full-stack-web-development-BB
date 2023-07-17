3deccbe200d1458d5590fd5c20724df858c9781b
me6edi <exited17@gmail.com>
2023-07-17 21:45:10 +0600

    Examples

--- Modern JavaScript Programming/Event/js/script.js
+++ Modern JavaScript Programming/Event/js/script.js
@@ -371,13 +371,36 @@
 // str = "+8801758865276";
 
 
-console.log(re.exec(str));
-reTest(re, str);
-function reTest(re, str) {
-    if(re.test(str)) {
-        console.log(`'${str}' matches '${re.source}'`);
-    }
-    else {
-        console.log(`'${str}' doesn't match '${re.source}'`);
-    }
-} 
+// console.log(re.exec(str));
+// reTest(re, str);
+// function reTest(re, str) {
+//     if(re.test(str)) {
+//         console.log(`'${str}' matches '${re.source}'`);
+//     }
+//     else {
+//         console.log(`'${str}' doesn't match '${re.source}'`);
+//     }
+// } 
+
+
+// Some Examples Regular Expression
+// Examples
+// let re;
+// let str;
+
+// // Postal Code
+// // 4700, 4000
+// re = /^[0-9]{4}$/;
+// str = "4000";
+
+// // Phone Number
+// // 01717888888 +8801717888888 8801717888888
+// re = /^(+)?(88)?01[0-9]{9}$/;
+// str = "+8801717888888";
+
+// // Email Address
+// // bohubrihi8.learn@edu.com.bd
+// re = /^([a-zA-Z0-9].?)+[^.]@([a-zA-Z0-9].?)+[^.]$/ ;
+// str = "bohubrihi.learn@edu.com.bd" ;
+
+// console.log(re.test(str)); 
