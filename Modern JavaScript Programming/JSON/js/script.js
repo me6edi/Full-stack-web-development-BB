df285fb5719386f7b536a1d5b3eb8cc84102f0b1
me6edi <exited17@gmail.com>
2023-07-21 21:12:38 +0600

    AJAX

--- Modern JavaScript Programming/JSON/js/script.js
+++ Modern JavaScript Programming/JSON/js/script.js
@@ -6,22 +6,24 @@ xmlhttp.onreadystatechange = function () {
         jsonData(data);
     }
 };
+
+
 xmlhttp.open("GET", "data.json", true);
 xmlhttp.send();
 
 function jsonData(json_obj) {
     //console.log(json_obj);
     var js_obj = JSON.parse(json_obj);
-    console.log(js_obj);
 
-    // for (x in js_obj.persons) {
-    //     //console.log(x);
+    for (x in js_obj.persons) {
+        // console.log(x);
 
-    //     var persons = js_obj.persons;
-    //     // console.log(persons[x]);
-    //     for (y in persons[x]) {
-    //         console.log(persons[x][y]);
-    //     }
+        var persons = js_obj.persons; 
+        // console.log(persons[x]);
+        for (y in persons[x]) {
+            console.log(persons[x][y]);
+        }
+    }
 
-    // }
 }
+
