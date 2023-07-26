ae4c9c183621f687126882a1969b3ad9f56b2774
me6edi <exited17@gmail.com>
2023-07-26 21:32:36 +0600

    callback function

--- Modern JavaScript Programming/Callback Function/js/script.js
+++ Modern JavaScript Programming/Callback Function/js/script.js
@@ -4,23 +4,49 @@
 //     console.log("Hello World!");
 // }, 5000);
 
-let persons = [
-    {firstName: "Simanta", lastName: "Paul"},
-    {firstName: "Fazle", lastName: "Rahat"}
-]
+// let persons = [
+//     {firstName: "Mehedi", lastName: "Amin"},
+//     {firstName: "Robin", lastName: "Hassan"}
+// ]
 
-function createPerson(person) {
-   setTimeout(function() {
-       persons.push(person);
-   }, 4000); 
-}
+// function createPerson(person) {
+//    setTimeout(function() {
+//        persons.push(person);
+//    }, 4000); 
+// }
 
-function getPerson() {
-    setTimeout(function(){
-        let output = '';
-        persons.forEach(function(person){
-            output += `<li>${person.firstName} ${person.lastName}</li>`
-        }); 
-        document.getElementById('output').innerHTML = output;
-    }, 1000);
-}
+// function getPerson() {
+//     setTimeout(function(){
+//         let output = '';
+//         persons.forEach(function(person){
+//             output += `<li>${person.firstName} ${person.lastName}</li>`
+//         }); 
+//         document.getElementById('output').innerHTML = output;
+//     }, 1000);
+// }
+
+// let persons = [
+//     {firstName: "Simanta", lastName: "Paul"},
+//     {firstName: "Fazle", lastName: "Rahat"}
+// ]
+
+// function createPerson(person, callback) {
+//    setTimeout(function() {
+//        persons.push(person);
+//        callback();
+//    }, 2000); 
+// }
+
+// function getPerson() {
+//     setTimeout(function(){
+//         let output = '';
+//         persons.forEach(function(person){
+//             output += `<li>${person.firstName} ${person.lastName}</li>`
+//         }); 
+//         document.getElementById('output').innerHTML = output;
+//     }, 500);
+// }
+
+// createPerson({firstName:"Rony", lastName: "Chy"}, getPerson);
+// getPerson();
+
