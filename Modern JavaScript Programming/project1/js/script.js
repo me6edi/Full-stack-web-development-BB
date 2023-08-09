ecfb859b887e61f7ae0225f987da16ed9117da3e
me6edi <exited17@gmail.com>
2023-08-09 20:41:39 +0600

    local storage

--- Modern JavaScript Programming/project1/js/script.js
+++ Modern JavaScript Programming/project1/js/script.js
@@ -9,7 +9,8 @@ let taskInput = document.querySelector('#new_task');
 // Define event listeners
 form.addEventListener('submit', addTask);
 taskList.addEventListener('click', removeTask);
-clearBtn.addEventListener('click', clearTask)
+clearBtn.addEventListener('click', clearTask);
+filter.addEventListener('keyup', filterTask);
 // Define function 
 // Add Task
 function addTask(e){
@@ -18,12 +19,15 @@ function addTask(e){
     }else{
         // create li element
         let li = document.createElement('li');
-        li.appendChild(document.createTextNode(taskInput.value + ''));
+        li.appendChild(document.createTextNode(taskInput.value + ' '));
         let link = document.createElement('a');
         link.setAttribute('href', '#');
         link.innerHTML = 'x'
         li.appendChild(link);
         taskList.appendChild(li);
+
+        storeTaskInLocalstorage(taskInput.value)
+
         taskInput.value = '';
     }
     e.preventDefault();
@@ -44,4 +48,34 @@ function removeTask(e) {
 function clearTask(e){
     taskList.innerHTML = '';
     
+}
+
+
+// Filter Task 
+
+function filterTask(e){
+    let text = e.target.value.toLowerCase();
+    document.querySelectorAll('li').forEach(task => {
+        let item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text)!= -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    })
+}
+
+
+// Store in local storage
+
+function storeTaskInLocalstorage(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
