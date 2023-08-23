eb9f8fddaff9eb4855c58e6c7a85ecda198aee22
me6edi <exited17@gmail.com>
2023-08-23 23:16:40 +0600

    Arrow Function (ES6)

--- Modern JavaScript Programming/Leveling Up to ES6/js/js/style.js
+++ Modern JavaScript Programming/Leveling Up to ES6/js/js/style.js
@@ -33,3 +33,57 @@ for (let a = 0; a <10; a ++) {
 
 
 console.log(`Global Scope: `, a, b, c); 
+
+
+
+// Normal Declaration
+function saySomething(name) {
+    console.log('Hello ' + name);
+}
+
+// Function Expression
+
+let saySomething1 = function(name2) {
+    console.log('Hello ' + name2);
+}
+
+// Arrow Function (ES6)
+
+let saySomething2 = (name3) => {
+    console.log("I am " + name3);
+}
+
+
+
+
+saySomething("Bohubrihi");
+saySomething1("Simanta");
+saySomething2("JavaScript"); 
+
+// Both of them are same
+
+let getsquare = num => {return num * num};
+let getsquare2 = num => num * num;
+console.log(getsquare(4));
+console.log(getsquare2(4));
+
+
+function myFunction(){
+    console.log("Simple function ");
+}
+
+myFunction();
+
+let myFunction2 = (name, dob) => {
+    console.log(name, dob);
+}
+
+myFunction2("name","08-26-1997");
+
+// call back function
+
+number = [1,2,3,4,5,6];
+let printAll = (item) => {
+    console.log(item);
+}
+number.forEach(printAll);
