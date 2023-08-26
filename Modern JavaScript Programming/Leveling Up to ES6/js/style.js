cf7a67841131496f10ba5934fca5d9064b4fd19b
me6edi <exited17@gmail.com>
2023-08-26 14:35:08 +0600

    Spread Operator

--- Modern JavaScript Programming/Leveling Up to ES6/js/style.js
+++ Modern JavaScript Programming/Leveling Up to ES6/js/style.js
@@ -155,3 +155,39 @@
 //  let newPerson = {...person, dob: "08-26-1995"}
 //  console.log(newPerson);
 
+
+// Spread Operator ...
+// let str = "Bohubrihi";
+// let newStr = [...str];
+
+// console.log(newStr);
+
+// let fruit1 = ["Apple", "Pine-apple", "Mango"];
+// let fruit2 = ["Orange", "Grape"];
+// let newFruit = "Jackfruit";
+
+// let newArr = [...fruit1,newFruit, ...fruit2];
+
+// console.log(newArr);
+// Spread on Object (ES8)
+// let person = {
+//     fname: "Simanta",
+//     lname: "Paul"
+// }
+
+// let newPerson = {...person, dob: "08-26-1995"}
+// console.log(newPerson);
+
+// Spread Operator ...
+
+// let numbers = [23, 1, 0, -1];
+
+//console.log(Math.max(...numbers));
+
+// let person = ["Mehedi", "Amin"]
+
+// let test = (fname, lname) => {
+//     console.log(`Hello ${fname} ${lname}`);
+// }
+
+// test(...person); 
