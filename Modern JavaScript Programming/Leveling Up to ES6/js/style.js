cd13ed47abb15fba73bdcf72d06b9e5aa454cb05
me6edi <exited17@gmail.com>
2023-08-27 22:08:15 +0600

    Before ES6 Classes

--- Modern JavaScript Programming/Leveling Up to ES6/js/style.js
+++ Modern JavaScript Programming/Leveling Up to ES6/js/style.js
@@ -191,3 +191,90 @@
 // }
 
 // test(...person); 
+
+
+
+
+// Rest Operator ...
+
+// let fruits = ['apple', 'grape', 'mango', 'jackfruit'];
+// let [first, second, ...third] = fruits
+
+// // console.log(first);
+// // console.log(second);
+// // console.log(third);
+
+// let person = {
+//     fname: "Simanta",
+//     lname: "Paul",
+//     dob: "8-26-1995"
+// }
+
+// let {fname, ...lname} = person
+
+// // console.log(fname);
+// // console.log(lname);
+
+// let moreNum = [78,1,2,5,6];
+
+// let test =(name, ...numbers) => { // Rest
+//     console.log(name);
+//     console.log(numbers);
+// }
+
+// test("Simanta", 67,3,3);
+// test("Simanta", ...moreNum); // Spread 
+
+
+
+
+// Promises
+// An alternative to Callback
+
+// let applyDiscount = new Promise((resolve, reject) => {
+//     let discount = false;
+//     if (discount) {
+//         resolve('Discount Applied');
+//     } else {
+//         reject('Discount Failed!');
+//     }
+// });
+
+// applyDiscount
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(result => {
+//         console.log(result);
+//     })
+
+// //console.log(applyDiscount);
+
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise 
+
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+// let person1 = new Person("Mehedi", "23");
+
+// console.log(person1.name, person1.age);
+
+// ES6 Class
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getName(){
+        console.log(this.name);
+    }
+}
+
+
+
+
+let person2 = new Person("Mehedi", "23");
+
+console.log(person2.name, person2.age);
