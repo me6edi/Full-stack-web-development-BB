00a03233da8fa1ef82faafbee41d813bd72b7bb3
me6edi <exited17@gmail.com>
2023-08-29 21:40:27 +0600

    Symbols

--- Modern JavaScript Programming/Leveling Up to ES6/js/style.js
+++ Modern JavaScript Programming/Leveling Up to ES6/js/style.js
@@ -262,19 +262,49 @@
 
 // ES6 Class
 
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    getName(){
-        console.log(this.name);
-    }
-}
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     getName(){
+//         console.log(this.name);
+//     }
+// }
+
+// let person2 = new Person("Mehedi", "23");
+
+// console.log(person2.name, person2.age);
 
 
 
 
-let person2 = new Person("Mehedi", "23");
 
-console.log(person2.name, person2.age);
+ // Symbols
+// let a = Symbol()
+
+// let person = {
+//     name: "Simanta",
+//     age: 25,
+//     [a]: "Hello World!"
+// }
+
+// console.log(person);
+// console.log(Object.getOwnPropertyNames(person));
+// console.log(Object.getOwnPropertySymbols(person));
+// console.log(Object.keys(person));
+// console.log(JSON.stringify(person));
+
+
+// for (x in person) {
+//     console.log(x);
+
+// }
+
+// let sym1 = Symbol("Hello");
+// let sym2 = Symbol("Hello");
+
+// console.log(sym1 == sym2);
+// console.log(sym1);
+
+
