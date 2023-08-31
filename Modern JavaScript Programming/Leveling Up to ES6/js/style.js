705c0c4145cd19ad0a3d6ad6199abdf8061057a6
me6edi <exited17@gmail.com>
2023-08-31 22:56:01 +0600

    Async Await

--- Modern JavaScript Programming/Leveling Up to ES6/js/style.js
+++ Modern JavaScript Programming/Leveling Up to ES6/js/style.js
@@ -397,3 +397,22 @@
 // console.log("I am after Promise");
 
 // // Example : Image File 
+
+
+// Async Await
+
+// fetch('http://api.icndb.com/jokes/random/5000')
+//     .then(response => response.json())
+//     .then(data => { });
+
+
+// // async await
+
+// async function getJokes() {
+//     let response = await fetch('http://api.icndb.com/jokes/random/5000');
+//     let data = await response.json();
+//     return data;
+// }
+
+
+// getJokes().then(jokes => console.log(jokes)); 
