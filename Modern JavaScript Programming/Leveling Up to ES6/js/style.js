7f9c9bb1c688468f4e5021a60329e34af6ea2043
Mehedi <exited17@gmail.com>
2023-09-21 21:39:07 +0600

    add to card

--- Modern JavaScript Programming/Leveling Up to ES6/js/style.js
+++ Modern JavaScript Programming/Leveling Up to ES6/js/style.js
@@ -494,16 +494,71 @@
 
 
 
-let arr = [1,2,2,33,33]
-console.log(arr);
+// let arr = [1,2,2,33,33]
+// console.log(arr);
 
-let mySet = new Set([1, 2, 2, 4, 4]);
-mySet.add("Hello");
-mySet.add(4);
-mySet.delete(4);
+// let mySet = new Set([1, 2, 2, 4, 4]);
+// mySet.add("Hello");
+// mySet.add(4);
+// mySet.delete(4);
 
-let iter = [...mySet.entries()];
-console.log(iter);
+// let iter = [...mySet.entries()];
+// console.log(iter);
 
 
-mySet.forEach(value => console.log(value));
+// mySet.forEach(value => console.log(value));
+
+
+// Maps
+// let myMap = new Map(
+//     [
+//         ['first key', 10],
+//         ['second key', 'Bohubrihi']
+//     ]
+// );
+
+// // Add
+// myMap.set('third key', 'Hello Wolrd!');
+
+
+//console.log(myMap.get('second key'));
+//console.log(myMap.has('key'));
+//console.log(myMap.size);
+
+// Iterate
+
+// for (let x of myMap) {
+//     console.log(x);
+// }
+
+// for (let [x,y] of myMap) {
+//     console.log(x, y);
+
+// }
+
+// for (let x of myMap.keys()) {
+//     console.log(x);
+// }
+
+// for (let x of myMap.values()) {
+//     console.log(x);
+// }
+
+// for (let x of myMap.entries()) {
+//     console.log(x);
+// }
+
+// myMap.forEach((x, y) => {
+//     console.log(x,y);
+
+// })
+//let arr = Array.from(myMap);
+//let arr = Array.from(myMap.keys());
+// let arr = Array.from(myMap.values());
+// console.log(arr);
+
+// console.log(myMap.size);
+
+// for (let x of myMap.entries()) {
+//     console.log(x);
+// }
